9f554dcc324c24c89fa508bfd4decd9c161abc40
Watch348 <A25110166@ceti.mx>
2026-01-18 21:56:28 -0600

    Project Creation

---
+++ PassWOrdU/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace PassWOrdU
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Contraseña = new Label();
+            SuspendLayout();
+            // 
+            // Contraseña
+            // 
+            Contraseña.AutoSize = true;
+            Contraseña.Location = new Point(194, 98);
+            Contraseña.Name = "Contraseña";
+            Contraseña.Size = new Size(198, 48);
+            Contraseña.TabIndex = 0;
+            Contraseña.Text = "Contraseña";
+            Contraseña.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1792, 922);
+            Controls.Add(Contraseña);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Contraseña;
+    }
+}
---
+++ PassWOrdU/Form1.cs
@@ -0,0 +1,15 @@
+namespace PassWOrdU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
